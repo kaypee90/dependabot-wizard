a67c40b49dbeeadc556d735081a4caf0a16e551c
kaypee90 <kaypee90@yahoo.com>
2024-04-17 22:52:14 +0000

    Add intro text to cli

--- cmd/depbot/main.go
+++ cmd/depbot/main.go
@@ -1,8 +1,18 @@
 package main
 
+import "fmt"
+
 const fileName = "dependabot.yml"
 
+func printIntroText() {
+    green := "\033[32m"
+    reset := "\033[0m"
+    fmt.Printf("%sDepbot wizard will help you configure dependabot in your project%s\n", green, reset)
+}
+
 func main() {
+	printIntroText()
+
 	packageEcosystem := getPackageEcosystem()
 	directory := getDirectory()
 	interval := getInterval()
@@ -27,3 +37,4 @@ func main() {
 	yamlData := config.ConvertToYaml()
 	writeDataFile(fileName, yamlData)
 }
+
