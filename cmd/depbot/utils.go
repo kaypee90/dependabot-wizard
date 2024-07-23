17d563242e3143070860e682f36fd49ef9887802
kaypee90 <kaypee90@yahoo.com>
2024-07-23 00:23:51 -0400

    Refactor utils

--- cmd/depbot/utils.go
+++ cmd/depbot/utils.go
@@ -15,22 +15,7 @@ const (
 func displayIntroductoryText() {
 	green := "\033[32m"
 	reset := "\033[0m"
-	fmt.Printf("%sDepbot wizard will help you configure dependabot in your project%s\n", green, reset)
-}
-
-func createDirIfItDoesNotExit(dir string) (hasCreatedNewDir bool, err error) {
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		err := os.Mkdir(dir, 0755)
-
-		if err != nil {
-			log.Fatalf("Error creating directory %s: %v", dir, err)
-			return false, err
-		}
-
-		return true, nil
-	}
-
-	return false, nil
+	fmt.Printf("%sDepbot wizard will assist you to configure dependabot in your project%s\n", green, reset)
 }
 
 func writeDataToFile(fileName string, data []byte) {
@@ -48,6 +33,21 @@ func writeDataToFile(fileName string, data []byte) {
 	log.Printf("Dependabot config created successfully!")
 }
 
+func createDirIfItDoesNotExit(dir string) (hasCreatedNewDir bool, err error) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.Mkdir(dir, 0755)
+
+		if err != nil {
+			log.Fatalf("Error creating directory %s: %v", dir, err)
+			return false, err
+		}
+
+		return true, nil
+	}
+
+	return false, nil
+}
+
 func createConfigurationFile(fileName string, destinationDir string, data []byte, skipCreatingDir bool) {
 	fullFilePath := fileName
 
@@ -56,7 +56,7 @@ func createConfigurationFile(fileName string, destinationDir string, data []byte
 		createDirIfItDoesNotExit(destinationDir)
 		fullFilePath = destinationDir + "/" + fileName
 	} else {
-		log.Printf("Skipping directory creation since current directory is %s", githubDir)
+		log.Printf("Skipping directory creation since %s already exists", githubDir)
 	}
 
 	writeDataToFile(fullFilePath, data)
