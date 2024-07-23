2bec0f15109720efdd821f4bf4d204082c333000
kaypee90 <kaypee90@yahoo.com>
2024-07-23 00:34:20 -0400

    Clean up

--- cmd/depbot/utils.go
+++ cmd/depbot/utils.go
@@ -9,16 +9,16 @@ import (
 
 const (
 	dependabotFileName = "dependabot.yml"
-	githubDir          = ".github"
+	githubDirectory    = ".github"
 )
 
-func displayIntroductoryText() {
+func printIntroductoryText() {
 	green := "\033[32m"
 	reset := "\033[0m"
 	fmt.Printf("%sDepbot wizard will assist you to configure dependabot in your project%s\n", green, reset)
 }
 
-func writeDataToFile(fileName string, data []byte) {
+func writeBytesToFile(fileName string, data []byte) {
 	file, err := os.Create(fileName)
 	if err != nil {
 		log.Fatalf("Error creating file %s: %v", fileName, err)
@@ -33,7 +33,7 @@ func writeDataToFile(fileName string, data []byte) {
 	log.Printf("Dependabot config created successfully!")
 }
 
-func createDirIfItDoesNotExit(dir string) (hasCreatedNewDir bool, err error) {
+func createDirectoryIfItDoesNotExist(dir string) (hasCreatedNewDir bool, err error) {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		err := os.Mkdir(dir, 0755)
 
@@ -52,26 +52,26 @@ func createConfigurationFile(fileName string, destinationDir string, data []byte
 	fullFilePath := fileName
 
 	if !skipCreatingDir {
-		log.Printf("Creating %s directory..", githubDir)
-		createDirIfItDoesNotExit(destinationDir)
+		log.Printf("Creating %s directory..", githubDirectory)
+		createDirectoryIfItDoesNotExist(destinationDir)
 		fullFilePath = destinationDir + "/" + fileName
 	} else {
-		log.Printf("Skipping directory creation since %s already exists", githubDir)
+		log.Printf("Skipping directory creation since %s already exists", githubDirectory)
 	}
 
-	writeDataToFile(fullFilePath, data)
+	writeBytesToFile(fullFilePath, data)
 }
 
-func getCurrentDir() string {
-	currentDir, err := os.Getwd()
+func getWorkingDirectory() string {
+	currentDirectory, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
 
-	return filepath.Base(currentDir)
+	return filepath.Base(currentDirectory)
 }
 
-func createDependabotYmlFile(data []byte) {
-	skipCreatingDir := getCurrentDir() == githubDir
-	createConfigurationFile(dependabotFileName, githubDir, data, skipCreatingDir)
+func createDependabotYamlFile(data []byte) {
+	skipCreatingDir := getWorkingDirectory() == githubDirectory
+	createConfigurationFile(dependabotFileName, githubDirectory, data, skipCreatingDir)
 }
