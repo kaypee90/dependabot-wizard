389adf95853d0bb837e2a47d0c464fa562206d5b
kaypee90 <kaypee90@yahoo.com>
2024-12-15 00:45:23 -0500

    Make test explicit

--- github/graphqlclient_test.go
+++ github/graphqlclient_test.go
@@ -8,6 +8,9 @@ import (
 
 func TestGetPullRequests(t *testing.T) {
 	token := ""
+	if token == "" {
+		t.Skip("Skipping explicit test. Set gitub token to run this test.")
+	}
 	response := GetPullRequests("kaypee90", "zebrok", token)
 	assert.Equal(t, len(response.Data.Repository.PullRequests.Edges) > 0, true)
 }
